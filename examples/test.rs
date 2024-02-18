ac3cb38577b31c64bade9332a7cb5694320b7771
ThatEpicBanana <thatepicbanana132@gmail.com>
2024-02-17 23:19:43 -0500

    example(test): example for new testing things

---
+++ examples/test.rs
@@ -0,0 +1,26 @@
+use sauti::player::prelude::*;
+use sauti::test::prelude::*;
+
+fn main() {
+    // Create a sound packet with two frames and two channels
+    let packet = SoundPacket::from_channels(&[[1, 2], [3, 4]], 44100);
+    let generic = GenericPacket::from(packet);
+    // Use [`Collector`] to take the first two frames
+    let (collector, handle) = Collector::take(2);
+    // Use [`Provider`] to provide the packet to the collector
+    let provider = Provider::provide(generic.clone());
+
+    // Create a [`Player`] using the [`Collector`] and [`Provider`]
+    let player = Player::default_builder()
+        .decoder(provider)
+        .audio(collector)
+        // The SampleFormat has to be I32 because that's what's used above
+        .options(DeviceOptions::default().with_sample_format(SampleFormat::I32))
+        .run();
+    // Start decoding (and providing the data)
+    // [`Provider`] ignores the [`MediaSource`], so just send an empty path
+    player.play("").expect("failed to start sending data");
+
+    let given_packet = handle.collect();
+    assert_eq!(given_packet, generic);
+}
